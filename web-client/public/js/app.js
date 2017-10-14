ebf6ce603d28ed83d3aa176417b7d2c30d080519
Felix <felix.gerschau@gmail.com>
2017-10-14 14:41:46 +0200

    Add selectpicker for languages

--- web-client/public/js/app.js
+++ web-client/public/js/app.js
@@ -1,2 +1,7 @@
 var seMiProfeApp = angular.module('seMiProfeApp', ['ui.bootstrap']);
 var apiUrl = 'http://localhost:3000';
+
+var LANGUAGETRANSLATION = {
+  'es-ES': 'Español',
+  'en-GB': 'Inglés británico',
+};

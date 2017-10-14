bb3513db7ba0b3c8d065be4e15adc15073dfc268
Felix <felix.gerschau@gmail.com>
2017-10-14 13:41:43 +0200

    Add pagination and change displayed data

--- web-client/public/js/controllers/search.js
+++ web-client/public/js/controllers/search.js
@@ -1,18 +1,58 @@
 seMiProfeApp.controller('searchController', function ($scope, userService) {
   var SIZE = 10;
+  $scope.MAXPAGES = 5;
   $scope.page = 1;
+  $scope.tableData = [];
 
-  function getTeachers() {
+  function getPaginationArray(from, to) {
+    if (((from - 1) % $scope.MAXPAGES !== 0 || to % $scope.MAXPAGES !== 0)) {
+      return $scope.pageArray;
+    }
+
+    from = from > 0 ? from : 1;
+    to = to < $scope.totalPages ? to : $scope.totalPages;
+    from = to - from <= $scope.MAXPAGES ? from : to - $scope.MAXPAGES;
+
+    var pages = [];
+    for (var i = from; i <= to; i++) {
+      pages.push(i);
+    }
+
+    $scope.pageArray = pages;
+    return pages;
+  }
+
+  $scope.getPaginationArray = getPaginationArray;
+
+  function getTeachers(updatePagination) {
     $scope.filter = {
-      page: $scope.page,
+      page: $scope.page - 1,
       size: SIZE,
+      search: $scope.searchQuery,
     };
+
+    $scope.tableData = [];
 
     userService.get($scope.filter).then(function (data) {
       $scope.tableData = data.content;
-      $scope.total = data.totalElements;
+      $scope.totalPages = data.totalPages;
+      $scope.totalElements = data.totalElements;
+      if (updatePagination) {
+        getPaginationArray(1, $scope.MAXPAGES);
+      }
     });
   }
 
-  getTeachers();
+  getTeachers(true);
+
+  $scope.search = function () {
+    getTeachers(true);
+  };
+
+  function goToPage(page) {
+    $scope.page = page;
+    getTeachers(false);
+  }
+
+  $scope.goToPage = goToPage;
 });
--- web-client/public/js/services/user.js
+++ web-client/public/js/services/user.js
@@ -4,6 +4,6 @@ seMiProfeApp.service('userService', function ($http) {
   }
 
   this.get = function (options) {
-    return $http.get(apiUrl + '/user', options).then(responseData);
+    return $http.get(apiUrl + '/user', { params: options }).then(responseData);
   };
 });

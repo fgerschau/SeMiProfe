df58bea3a66447d372c6378c26f949972954c66f
Felix <felix.gerschau@gmail.com>
2017-10-14 13:41:08 +0200

    Order by first name

--- api/src/main/java/api/repository/UserRepository.java
+++ api/src/main/java/api/repository/UserRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User u WHERE u.isTeacher = ?1 and (LOWER(u.firstName) LIKE LOWER(CONCAT('%', ?2, '%')) or LOWER(u.lastName) LIKE LOWER(CONCAT('%', ?3, '%')) or LOWER(u.language) LIKE LOWER(CONCAT('%', ?4, '%')))")
+    @Query("SELECT u FROM User u WHERE u.isTeacher = ?1 and (LOWER(u.firstName) LIKE LOWER(CONCAT('%', ?2, '%')) or LOWER(u.lastName) LIKE LOWER(CONCAT('%', ?3, '%')) or LOWER(u.language) LIKE LOWER(CONCAT('%', ?4, '%'))) ORDER BY u.firstName")
     Page<User> findAll(Boolean isTeacher, String firstName, String lastName, String language, Pageable pageable);
 }

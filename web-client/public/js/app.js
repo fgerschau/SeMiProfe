86ee58599b206b4f63f872f1cd2c48f4af8d21c2
Lewis135 <lewislopezmontaner@gmail.com>
2017-10-17 15:51:55 +0200

    update

--- web-client/public/js/app.js
+++ web-client/public/js/app.js
@@ -1,2 +1,8 @@
 var seMiProfeApp = angular.module('seMiProfeApp', ['ui.bootstrap']);
-var apiUrl = 'http://localhost:3000';
+var apiUrl = window.location.href.match(/^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/g);
+apiUrl = apiUrl + ":3000";
+
+var LANGUAGETRANSLATION = {
+  'es-ES': 'Español',
+  'en-GB': 'Inglés británico',
+};

79b5661cce6c3e7a9a71840d4b9ad54a7d17fa07
Lewis135 <lewislopezmontaner@gmail.com>
2017-10-17 15:54:20 +0200

    Not sure if it worrks, i dont remind it

--- api/src/main/java/api/Application.java
+++ api/src/main/java/api/Application.java
@@ -1,5 +1,6 @@
 package api;
 
+import api.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +8,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
+
+		User user1 = new User("Paco", "Ramirez", "1234", "asd@gmail.com");
+		User user2 = new User("Paca", "Ramirez", "1111", "qwe@paco.com");
+		User user3 = new User("Pepe", "Lopez", "0000", "asd@gmail.com");
+		System.out.print("user4 mal el correo:  ");
+		User user4 = new User("Pepe", "Lopez", "0000", "asd@");
+
+
+		if (user1.equals(user2)) {
+			System.out.println("user1 y user2 Son iguales");
+		} else System.out.println("user1 y user2 NO Son iguales");
+
+		if (user1.equals(user3)) {
+			System.out.println("user1 y user3 Son iguales");
+		} else System.out.println("user1 y user3 NO Son iguales");
+
+
 	}
 }
--- api/src/main/java/api/model/User.java
+++ api/src/main/java/api/model/User.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.awt.Image;
+
+
 
 @Entity
 public class User {
@@ -13,13 +16,40 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+    private String password;
+    private String email;
+    private Image image;
 
     protected User() {}
 
-    public User(String firstName, String lastName) {
+    public User(String firstName, String lastName, String _password, String _email)  {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.password = _password;
+        if (checkEmail(_email))
+            this.email = _email;
+        else System.out.println("Aqui deberia ir un error, pero esta comprobacion se hace en el frontend");
+        this.email = _email;
+
     }
+
+    public boolean checkUserPassword(String _pass){
+        if (password.equals(_pass)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    private boolean checkEmail(String _email){
+        String fin = _email.substring(_email.indexOf('@'), _email.length());
+        if (fin.length() >= 3) { // At least x.y
+            return true;
+        }
+        return false;
+
+    }
+
 
     @Override
     public String toString() {
@@ -29,12 +59,28 @@ public class User {
 	public Long getId() {
 		return id;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
+    public boolean equals(User u) {
+        if (this.email.equals(u.getEmail())){
+            return true;
+        }
+        return false;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

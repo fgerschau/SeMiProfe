b501eda9eed718e26425e06026f0b0114f5cc0d9
Felix <felix.gerschau@gmail.com>
2017-10-25 19:09:46 +0200

    Reset backend changes to dev branch

--- api/src/main/java/api/model/User.java
+++ api/src/main/java/api/model/User.java
@@ -4,9 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.awt.Image;
-
-
 
 @Entity
 public class User {
@@ -16,42 +13,17 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
-    private String email;
-    private Image image;
     private Boolean isTeacher;
     private String language;
+    private String email;
     private String password;
 
     protected User() {}
 
-    public User(String firstName, String lastName, String _password, String _email)  {
+    public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.password = _password;
-        if (checkEmail(_email))
-            this.email = _email;
-        else System.out.println("Aqui deberia ir un error, pero esta comprobacion se hace en el frontend");
-        this.email = _email;
-
     }
-
-    public boolean checkUserPassword(String _pass){
-        if (password.equals(_pass)) {
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-    private boolean checkEmail(String _email){
-        String fin = _email.substring(_email.indexOf('@'), _email.length());
-        if (fin.length() >= 3) { // At least x.y
-            return true;
-        }
-        return false;
-
-    }
-
 
     @Override
     public String toString() {
@@ -61,33 +33,14 @@ public class User {
 	public Long getId() {
 		return id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
-    public boolean equals(User u) {
-        if (this.email.equals(u.getEmail())){
-            return true;
-        }
-        return false;
-    }
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public Boolean getIsTeacher() {
         return isTeacher;
@@ -96,4 +49,13 @@ public class User {
     public String getLanguage() {
         return language;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }

0c40587c51a80f14ad91591dc76ca769978f7e5b
Felix <felix.gerschau@gmail.com>
2017-11-19 12:26:53 +0100

    Create CERF-level table and relation

--- api/src/main/java/api/model/User.java
+++ api/src/main/java/api/model/User.java
@@ -1,34 +1,28 @@
 package api.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class User {
-
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
-    @Column(nullable = false)
     private String firstName;
-    @Column(nullable = false)
     private String lastName;
-    @Column(nullable = false)
     private Boolean isTeacher;
     private String language;
-    @Column(nullable = false, unique = true)
     private String email;
-    @Column(nullable = false)
     private String password;
     private int phone;
-    @Column(nullable = false)
     private String province;
     private String town;
+    private Set<CEFRLevel> cefrLevels;
 
     protected User() {}
 
-    public User(String firstName, String lastName) {
+    public User(String firstName, String lastName, Set<CEFRLevel> cefrLevels) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.cefrLevels = cefrLevels;
     }
 
     @Override
@@ -36,32 +30,67 @@ public class User {
         return String.format("User[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
     }
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
 	public Long getId() {
 		return id;
 	}
 
-	public String getFirstName() {
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(nullable = false)
+    public String getFirstName() {
 		return firstName;
 	}
 
-	public String getLastName() {
+	public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(nullable = false)
+    public String getLastName() {
 		return lastName;
 	}
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(nullable = false)
     public Boolean getIsTeacher() {
         return isTeacher;
+    }
+
+    public void setIsTeacher(Boolean isTeacher) {
+        this.isTeacher = isTeacher;
     }
 
     public String getLanguage() {
         return language;
     }
 
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Column(nullable = false, unique = true)
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(nullable = false)
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public int getPhone() {
@@ -72,6 +101,7 @@ public class User {
         this.phone = phone;
     }
 
+    @Column(nullable = false)
     public String getProvince() {
         return province;
     }
@@ -87,5 +117,15 @@ public class User {
 
     public void setTown(String town) {
         this.town = town;
+    }
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_CEFRLevel", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "cefrlevel_id", referencedColumnName = "id"))
+    public Set<CEFRLevel> getCefrLevels() {
+        return cefrLevels;
+    }
+
+    public void setCefrLevels(Set<CEFRLevel> cefrLevels) {
+        this.cefrLevels = cefrLevels;
     }
 }

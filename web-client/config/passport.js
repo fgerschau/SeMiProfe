4f6aece48b7a984a5ffb869434b035b9997aec4c
Felix <felix.gerschau@gmail.com>
2017-12-12 17:41:56 +0100

    Improve deserialization error prevention

--- web-client/config/passport.js
+++ web-client/config/passport.js
@@ -10,11 +10,14 @@ module.exports = function (passport) {
 
   passport.deserializeUser(function (email, done) {
     userController.getByEmail(email).then(function (user) {
-      if (!user) {
+      if (!user || typeof user !== 'object' || !user.id) {
         done(null, null);
       }
 
       done(null, user);
+    }).catch(function (err) {
+      console.log(err);
+      done(null, null);
     });
   });
 

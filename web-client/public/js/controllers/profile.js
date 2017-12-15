23229c24bff12a0675f471aef57cca3247ef3541
Felix <felix.gerschau@gmail.com>
2017-12-15 17:38:30 +0100

    Show only 5 reviews with the options to see more

--- web-client/public/js/controllers/profile.js
+++ web-client/public/js/controllers/profile.js
@@ -1,17 +1,23 @@
 seMiProfeApp.controller('profileController', function ($scope, userService, reviewService) {
+  var NREVIEWS = 5;
+
   $scope.init = function (selectedUser) {
+    $scope.showMore = true;
+    $scope.NREVIEWS = NREVIEWS;
     $scope.selectedUser = selectedUser;
     userService.getByEmail(selectedUser.email).then(function (user) {
       $scope.selectedUser = user;
+      $scope.reviews = $scope.selectedUser.receivedReviews.slice(0, NREVIEWS);
       userService.getLoggedUser().then(function (data) {
         $scope.user = data;
       }).catch(console.log);
     }).catch(console.log);
   };
 
-  function reload() {
+  function reload(nReviews) {
     userService.getByEmail($scope.selectedUser.email).then(function (user) {
       $scope.selectedUser = user;
+      $scope.reviews = $scope.selectedUser.receivedReviews.slice(0, NREVIEWS);
     }).catch(console.log);
   }
 
@@ -24,5 +30,20 @@ seMiProfeApp.controller('profileController', function ($scope, userService, revi
     };
 
     reviewService.save(review).then(reload);
+  };
+
+  function changeReviewsShowed(nReviews) {
+    nReviews = nReviews === -1 ? undefined : NREVIEWS;
+    $scope.reviews = $scope.selectedUser.receivedReviews.slice(0, nReviews);
+  }
+
+  $scope.toggleShowMore = function () {
+    if ($scope.showMore) {
+      changeReviewsShowed(-1);
+    } else {
+      changeReviewsShowed();
+    }
+
+    $scope.showMore = !$scope.showMore;
   };
 });

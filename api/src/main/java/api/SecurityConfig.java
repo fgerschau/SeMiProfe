0b01f3eee120935a0cc474b9e8d26e18bfc84b95
Felix <felix.gerschau@gmail.com>
2017-12-15 17:13:02 +0100

    Delete unused import

--- api/src/main/java/api/SecurityConfig.java
+++ api/src/main/java/api/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;

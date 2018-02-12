3836401ddbe676099f2865d2485e5ccffeb7e28f
Felix <felix.gerschau@gmail.com>
2018-02-12 18:47:56 +0100

    Fix problem in first react root render

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  React.createElement(BrowserRouter, React.createElement(App)),
+  React.createElement(BrowserRouter, {}, React.createElement(App)),
   document.getElementById('root'),
 );
 registerServiceWorker();

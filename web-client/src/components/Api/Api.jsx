f1efca61b15457ecfb7bd76c265ce10bc0a092db
Felix <felix.gerschau@gmail.com>
2018-02-18 12:47:15 +0100

    Add Api component

---
+++ web-client/src/components/Api/Api.jsx
@@ -0,0 +1,49 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import config from 'config/config.json';
+import Loading from './components/Loading';
+
+axios.defaults.baseURL = config.apiURL;
+
+function handleResponse(response) {
+  this.setState({ loading: false, data: response.data });
+  this.setData(response.data);
+  return response.data;
+}
+
+class Api extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+    const { setData } = props;
+    this.setData = setData;
+  }
+
+  loadTeachers() {
+    this.setData([]);
+    this.setState({ loading: true });
+    return axios.get('/user')
+      .then(handleResponse.bind(this))
+      .catch(() => { this.setState({ loading: false }); });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {this.state.loading ? <Loading /> : this.props.children}
+      </Fragment>
+    );
+  }
+}
+
+Api.propTypes = {
+  setData: PropTypes.func.isRequired,
+  children: PropTypes.node,
+};
+
+Api.defaultProps = {
+  children: <div />,
+};
+
+export default Api;
